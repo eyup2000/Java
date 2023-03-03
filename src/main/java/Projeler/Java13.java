4f4442e211045bbab39c24b986f6274ccb38735b
eyup2000 <gulerr.eyup47@gmail.com>
2023-03-03 16:36:07 +0300

    Java

---
+++ src/main/java/Projeler/Java13.java
@@ -0,0 +1,29 @@
+package Projeler;
+
+import java.util.Scanner;
+
+public class Java13 {
+    public static void main(String[] args) {
+        /*
+        girilen sayı çiftmi tekmi?
+
+         */
+        Scanner scanner = new Scanner(System.in);
+        int dongu = 10;
+
+        for (int i = 0; i<dongu; i++){
+
+            System.out.println("bir sayı giriniz");
+            int sayı = scanner.nextInt();
+
+            if (sayı % 2 == 0) {
+                System.out.println(sayı + " çift bir sayıdır!!");
+            }
+            else {
+                System.out.println(sayı + " tek bir sayıdır!!");
+            }
+
+        }
+
+    }
+}
