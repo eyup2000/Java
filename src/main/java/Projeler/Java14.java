9def9efcd9efd1fa2268fd65ded4f8870b5d4e25
eyup2000 <gulerr.eyup47@gmail.com>
2023-03-15 22:50:47 +0300

    Java

---
+++ src/main/java/Projeler/Java14.java
@@ -0,0 +1,22 @@
+package Projeler;
+
+import java.util.Scanner;
+
+public class Java14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] arr = new int[]{1,5,6,9,4,8,3,2,4,};
+        System.out.println("Bir sayı giriniz : ");
+        int aranacakSayı= scanner.nextInt();
+
+        for(int sayı : arr){
+            if(sayı==aranacakSayı){
+                System.out.println(aranacakSayı+" sayısı mevcuttur");
+                break;
+            }
+
+        }
+
+
+    }
+}
